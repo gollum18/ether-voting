e70e7805986006b0c387f95740e2b36235d2aa23
Christen Ford <christen0ford@gmail.com>
2019-12-07 22:52:44 -0500

    Update app.js

--- complete/src/js/app.js
+++ complete/src/js/app.js
@@ -8,20 +8,13 @@ App = {
   },
 
   initWeb3: async function() {
-    if (window.ethereum) {
-      App.web3Provider = new Web3(ethereum);
-      web3.setProvider(App.web3Provider);
-      try {
-        await ethereum.enable();
-      } catch (error) {
-        console.warn(error);
-      }
-    }
-    else if (window.web3) {
-      App.web3Provider = new web3.providers.HttpProvider('http://127.0.0.1:7545');
-      web3.setProvider(App.web3Provider);
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
     } else {
-      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+      // If no injected web3 instance is detected, fallback to Ganache.
+      App.web3Provider = new web3.providers.HttpProvider('http://127.0.0.1:7545');
+      web3 = new Web3(App.web3Provider);
     }
     web3.eth.getAccounts(function(error, accounts) {
       if (error) {
