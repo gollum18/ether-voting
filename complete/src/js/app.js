3d68d4f782683165ba3f961d9e35aac24a65c242
Christen Ford <christen0ford@gmail.com>
2019-12-08 01:32:19 -0500

    Update app.js

--- complete/src/js/app.js
+++ complete/src/js/app.js
@@ -8,6 +8,19 @@ App = {
   },
 
   initWeb3: function() {
+    if (window.ethereum) {
+      App.web3Provider = new Web3(ethereum);
+      web3.setProvider(App.web3Provider);
+      try {
+        await ethereum.enable();
+      } catch (error) {
+        console.warn(error);
+      }
+    }
+    else if (window.web3) {
+      App.web3Provider = new web3.providers.HttpProvider('http://127.0.0.1:7545');
+      web3.setProvider(App.web3Provider);
+    }
     if (typeof web3 !== 'undefined') {
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
